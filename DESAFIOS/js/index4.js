3c4b103710485861fb858fd546dce81f8944be75
Alejandro Cuñe <alejandrocune97@gmail.com>
2022-05-18 23:11:00 -0300

    se termino V.4

---
+++ DESAFIOS/js/index4.js
@@ -0,0 +1,122 @@
+/*
+	Declaramos atributos de estado
+*/
+let hambre = 100;
+let higiene = 100;
+let sueño = 100;
+let diversion = 100;
+let social = 100;
+
+/*
+	Esta función tiene la tarea de actualizar la información en pantalla
+de vez en cuando
+*/
+function Loop(time = 2){
+
+  let total = hambre + higiene + sueño + diversion + social;
+
+/*
+ Si alguno de los atributos llega a 0 nuestra mascota muere!
+a continuación controlamos el nivel de satisfacción de la mascota
+*/
+  if(hambre <= 0 || higiene <= 0 || sueño <= 0 || diversion <= 0 || social <= 0){
+     document.getElementById("pet").src="images/perrito0.jpg";
+     document.getElementById('mensaje').innerHTML = 'Dejaste morir a tu mascota, refresca la página';
+  }
+  else if(total > 400){
+     document.getElementById("pet").src="images/perrito100.jpg";
+  }
+  else if(total > 300){
+     document.getElementById("pet").src="images/perrito80.jpg";
+  }
+  else if(total > 200){
+     document.getElementById("pet").src="images/perrito60.jpg";
+  }
+  else if(total > 100){
+     document.getElementById("pet").src="images/perrito40.jpg";
+  }
+  else if(total > 50){
+     document.getElementById("pet").src="images/perrito20.jpg";
+  }
+
+  hambre = hambre - parseInt(time);
+  higiene = higiene - parseInt(time);
+  sueño = sueño - parseInt(time);
+  diversion = diversion - parseInt(time);
+  social = social - parseInt(time);
+
+  /*
+	 En esta parte controlamos la barra de estado de la mascota.
+  */
+  document.getElementById('hambre').style.width = hambre * 3 + 'px';
+  document.getElementById('higiene').style.width = higiene * 3 + 'px';
+  document.getElementById('sueño').style.width = sueño * 3 + 'px';
+  document.getElementById('diversion').style.width = diversion * 3 + 'px';
+  document.getElementById('social').style.width = social * 3 + 'px';
+
+  /*
+	 En esta parte controlamos el porcentaje del estado de la mascota
+  */
+  document.getElementById('hambre').innerHTML = hambre + '%';
+  document.getElementById('higiene').innerHTML = higiene + '%';
+  document.getElementById('sueño').innerHTML = sueño  + '%';
+  document.getElementById('diversion').innerHTML = diversion + '%';
+  document.getElementById('social').innerHTML = social + '%';
+}
+
+/*
+	Esta función es responsable de iniciar y llamar el temporizador de la función LOOP
+*/
+function Start(){
+  let temporizador = setInterval(Loop, 1000);
+}
+
+/*
+	Esta función es para recuperar el hambre.
+*/
+function Comer(){
+  hambre = 100;
+  document.getElementById('hambre').innerHTML = hambre + '%';
+  document.getElementById('hambre').style.width = hambre * 3 + 'px';
+}
+
+/*
+	Esta funcion es para recuperar higiene
+*/
+function Banio(){
+  higiene = 100;
+  document.getElementById('higiene').innerHTML = higiene + '%';
+  document.getElementById('higiene').style.width = higiene * 3 + 'px';
+}
+
+/*
+	Esta funcion es para recuperar sueño
+*/
+function Dormir(){
+  sueño = 100;
+  document.getElementById('sueño').innerHTML = sueño + '%';
+  document.getElementById('sueño').style.width = sueño * 3 + 'px';
+}
+
+/*
+	Esta funcion es para recuperar diversion
+*/
+function Brincar(){
+  diversion = 100;
+  document.getElementById('diversion').innerHTML = diversion + '%';
+  document.getElementById('diversionn').style.width = diversion * 3 + 'px';
+}
+
+/*
+	Esta funcion es para recuperar social
+*/
+function Conversar(){
+  social = 100;
+  document.getElementById('social').innerHTML = social + '%';
+  document.getElementById('social').style.width = social * 3 + 'px';
+}
+
+/*
+  Ejecutamos el temporizador
+*/
+Start();
