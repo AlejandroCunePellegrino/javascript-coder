7ff039216014211dbaed8ebb7d2d5ac03aa1ce55
AlejandroCunePellegrino <98554787+AlejandroCunePellegrino@users.noreply.github.com>
2022-06-20 19:12:46 -0300

    Delete nodos.js

--- DESAFIOS/js/nodos.js
+++
@@ -1,5 +0,0 @@
-let loginText = document.querySelector(".title-text .login");
-let loginForm = document.querySelector("form.login");
-let loginBtn = document.querySelector("label.login");
-let signupBtn = document.querySelector("label.signup");
-let signupLink = document.querySelector("form .signup-link a");
